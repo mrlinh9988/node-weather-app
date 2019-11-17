82605f4d4bc039d562f802a26879a893f396c420
mrlinh9988 <mrlinhdeptrai9988@gmail.com>
2019-11-17 11:49:28 +0700

    Add about text

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,14 @@
 
-fetch('http://localhost:3000/weather?address=Hanoi').then(res => {
-    res.json().then(data => {
-        if (data.error) {
-            console.log(data.error);
-        } else {
-            //console.log(data.location);
-            //console.log(data.forecast);
-        }
-    })
-})
+// fetch('http://localhost:3000/weather?address=Hanoi').then(res => {
+//     res.json().then(data => {
+//         if (data.error) {
+//             console.log(data.error);
+//         } else {
+//             //console.log(data.location);
+//             //console.log(data.forecast);
+//         }
+//     })
+// })
 
 
 
